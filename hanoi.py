b5b6a34b3235933de5befbf2b0fe86b10afd7140
lzx <112930061+NoExsisted@users.noreply.github.com>
2023-11-27 14:01:43 +0800

    Update hanoi.py

--- hanoi.py
+++ hanoi.py
@@ -2,12 +2,12 @@ import random
 def move(n, A, B, C, count=0):
     if n == 1:
         count += 1
-        # print(A, "->", C)
     else:
         count = move(n - 1, A, C, B, count)
         count += 1
         # print(A, "->", C)
         count = move(n - 1, B, A, C, count)
+        ## halo halo 删掉print(A, "->", C)
     return count
 
 
